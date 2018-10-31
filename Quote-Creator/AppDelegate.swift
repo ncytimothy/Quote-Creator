86c787b0826bdeb3a3227922801366c674813783
Timothy Ng <timothyng2008@me.com>
2018-10-31 21:07:20 +0800

    - fix: Fix QuoteTableVC Storyboard Class Linkage, TableView Delegate Methods Conformance

--- Quote-Creator/AppDelegate.swift
+++ Quote-Creator/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     // Singleton names (Single Source of Truth)
-    var names = [String]()
+    var quotes = [String]()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
---
+++ Quote-Creator/QuoteTableVC.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Quote-Creator
+//
+//  Created by Tim Ng on 10/31/18.
+//  Copyright © 2018 Learn Swift HK. All rights reserved.
+//
+
+import UIKit
+
+class QuoteTableVC: UIViewController {
+//-----------------------------------------------------------
+    // MARK: Properties
+    var quotes = [String]()
+    @IBOutlet var favoriteQuotesTable: UITableView!
+//-----------------------------------------------------------
+    // MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad called!")
+    }
+
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called!")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        quotes = appDelegate.quotes
+        favoriteQuotesTable.reloadData()
+        
+    }
+//-----------------------------------------------------------
+    // MARK: Actions
+    @IBAction func addQuote(_ sender: Any) {
+        let addQuoteVC = storyboard?.instantiateViewController(withIdentifier: "AddQuoteVC") as! UIViewController
+        navigationController?.pushViewController(addQuoteVC, animated: true)
+        
+    }
+    
+    
+    
+    
+//-----------------------------------------------------------
+    
+    
+
+}
+
+
+extension QuoteTableVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension QuoteTableVC: UITableViewDelegate {
+    
+}
+
