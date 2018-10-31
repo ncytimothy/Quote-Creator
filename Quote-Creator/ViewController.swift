736eee245940cfae4ef1f963a46e1f4d3339c692
Timothy Ng <timothyng2008@me.com>
2018-10-31 20:37:26 +0800

    Initial Commit

---
+++ Quote-Creator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quote-Creator
+//
+//  Created by Tim Ng on 10/31/18.
+//  Copyright © 2018 Learn Swift HK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
