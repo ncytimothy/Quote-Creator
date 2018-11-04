20abdfeb1ec0e0f802e909493307899d136a7096
Timothy Ng <timothyng2008@me.com>
2018-11-04 15:01:33 +0800

    - style: Clean up code for enter favorite quote UITextView

--- Quote-Creator/AddQuoteVC.swift
+++ Quote-Creator/AddQuoteVC.swift
@@ -10,26 +10,84 @@ import UIKit
 
 class AddQuoteVC: UIViewController {
     
+    //TODO:     1. Build the UITextView UI Interaction when user taps an empty area on screen, other than the UITextView itself
+    //TODO:    2. Build the IBAction for the "Add" button for the AddQuoteVC scene
+    //TODO:    3. Adjust cells dynamically on QuoteTableVC
+    
+//--------------------------------------------------------------------------------------------------------------------
+    // MARK: Properties
+    @IBOutlet weak var textView: UITextView!
+    
+//--------------------------------------------------------------------------------------------------------------------
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        textView.delegate = self
         
+        self.navigationItem.title = "Add Quote"
+        textView.layer.cornerRadius = 8.0
+        textView.layer.borderWidth = 2.0
+    
+        configureTapGestureRecognizer()
         
+    
         
     }
+    
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        configureTextViewPlaceholder()
+        
+    }
+    
+
+    
     
     @IBAction func cancelPressed(_ sender: Any) {
        self.navigationController?.popViewController(animated: true)
     }
     
+    
+//--------------------------------------------------------------------------------------------------------------------
+    // MARK: Helpers
+    
+    fileprivate func configureTapGestureRecognizer() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    fileprivate func configureTextViewPlaceholder() {
+        
+        let placeholder = "Enter Your Favorite Quote!"
+        textView.text = placeholder
+        textView.textColor = UIColor.gray
+    }
 }
-
-
+    
+    
 extension AddQuoteVC: UITextViewDelegate {
     
-    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        guard textView.textColor != UIColor.black else { return }
         textView.text = " "
-        return true
+        textView.textColor = UIColor.black
+        
     }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        guard textView.hasText else { return }
+        
+        configureTextViewPlaceholder()
+    }
+    
+    
     
 }
 
