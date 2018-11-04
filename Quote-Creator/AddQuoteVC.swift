a1b4e2fd2db5b5ee82b2c9a0b633ac62d3ced09d
Timothy Ng <timothyng2008@me.com>
2018-11-04 16:44:05 +0800

    - feat: Add screen displacement upwards when keyboard is shown and hidden methods

--- Quote-Creator/AddQuoteVC.swift
+++ Quote-Creator/AddQuoteVC.swift
@@ -20,28 +20,34 @@ class AddQuoteVC: UIViewController {
     
 //--------------------------------------------------------------------------------------------------------------------
     // MARK: Lifecycle
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
         
         self.navigationItem.title = "Add Quote"
-        textView.layer.cornerRadius = 8.0
-        textView.layer.borderWidth = 2.0
+        configureTextView()
     
         configureTapGestureRecognizer()
         
-    
-        
     }
-    
-
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         configureTextViewPlaceholder()
+        subscribeToKeyboardNotifications()
         
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboardNotifications()
+    }
+
+//--------------------------------------------------------------------------------------------------------------------
+    // MARK: Actions
     
 
     @IBAction func cancelPressed(_ sender: Any) {
@@ -67,19 +73,70 @@ class AddQuoteVC: UIViewController {
         textView.text = placeholder
         textView.textColor = UIColor.gray
     }
-}
-
+    
+    fileprivate func configureTextView() {
+        textView.layer.cornerRadius = 8.0
+        textView.layer.borderWidth = 2.0
+    }
+    
 //--------------------------------------------------------------------------------------------------------------------
     // MARK: Keyboard Methods
+    
+    // Idea of NSNotifications:
+    // When events happen, such as when the keyboard shows, an NSNotification will be posted across the system
+    // to the notification center
+    
+    // To listen for these notifications, we would need an observer
+    // We would subscribe to notifications when we need them
+    // Unsubscribe when it is no longer needed
+    
+    // If you know web development, this is similar to Event Listening on JQuery
+    
+    // TODO: Add observers to subscribe to notifications for .UIKeyboardWillShow and .UIKeyboardWillHide
+    // TODO: Configure the behavior of the keyboard when it shows and hides
+  
+    // 1. Add Observers to subscribe to keyboard notifications
+    // When the .UIKeyboardWillShow and .UIKeyboardWillHide notifications are
+    // triggered, the keyboardWillShow(_:) and keyboardWillHide(_:) functions
+    // are called
+    
+    // Note that keyboardWillShow(_:) and keyboardWillHide(_:) will take
+    // an NSNotification parameter which will be supplied by the observer
+    // and triggered by .UIKeyboardWillShow and .UIKeyboardWillHide
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // 2. Remove Observers for .UIKeyboardWillShow and .UIKeyboardWillHide
+    // This will be called when the view is going to disappear in
+    // viewWillDisappear()
 
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    // 3. Configure the screen movements when keyboard shows and hides
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        if textView.isFirstResponder {
+            view.frame.origin.y = -150
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        view.frame.origin.y = 0
+    }
+    
+    
 
+//-------------------------------------------------------------------------------------------------------------------
+}
 
-
-
-
-
-//--------------------------------------------------------------------------------------------------------------------
-    // MARK: UITextViewDelegate
+//-------------------------------------------------------------------------------------------------------------------
+    // MARK: Extension: UITextViewDelegate
 extension AddQuoteVC: UITextViewDelegate {
     
     func textViewDidBeginEditing(_ textView: UITextView) {
@@ -91,21 +148,16 @@ extension AddQuoteVC: UITextViewDelegate {
     
     func textViewDidEndEditing(_ textView: UITextView) {
         // Guard Staments: We guard based on a condition
-        // The condition will be the condition that we do not want the code to fall through
-        // after the guard statement
+       // We always guard the condition that is desirable
         
-        // Here when the textView hasText, we do not want to configure the Text View's placeholder
-        
-        // More precisely, we are saying that if textView.hasText is false,
-        // i.e. the textView does not have text, we will fall through
-        // and add the placeholder back into the Text View
+       // Here when textView hasText, we want to prevent falling thru
+      // when textView does NOT have text
+      
+     // Note that guard will fall thru when the condition is false
         guard textView.hasText else { return }
         
         configureTextViewPlaceholder()
     }
-    
-    
-    
 }
 
 
